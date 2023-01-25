c8fdca73bf6c014ef107b77d593b41b7dbd5c9c0
athekkep <athekkep@student.42abudhabi.ae>
2023-01-25 11:00:01 +0400

    ft_containers: removed comments

--- Circle 05/ft_containers/includes/iterators/iterator_traits.hpp
+++ Circle 05/ft_containers/includes/iterators/iterator_traits.hpp
@@ -17,14 +17,6 @@
 #include <iterator>	/* USED ONLY FOR ITERATOR TAGS */
 
 namespace ft {
-/* 		Tags__________________________________________________________________*/
-/*struct input_iterator_tag {};
-struct output_iterator_tag {};
-struct forward_iterator_tag 		: public input_iterator_tag {};
-struct bidirectional_iterator_tag 	: public forward_iterator_tag {};
-struct random_access_iterator_tag 	: public bidirectional_iterator_tag {};
-*/
-/* 		Traits________________________________________________________________*/
 template<class Iterator> struct iterator_traits {
 	typedef typename Iterator::difference_type		difference_type;
 	typedef typename Iterator::value_type			value_type;
@@ -48,17 +40,7 @@ template<class T> struct iterator_traits<const T*> {
 	typedef const T&						reference;
 	typedef std::random_access_iterator_tag	iterator_category;
 };
+
 };
-
-/*
-#include <typeinfo>     // typeid
-
-int main() {
-  typedef std::iterator_traits<int*> traits;
-  if (typeid(traits::iterator_category)==typeid(std::random_access_iterator_tag))
-    std::cout << "int* is a random-access iterator";
-  return 0;
-}
-*/
 
 #endif
